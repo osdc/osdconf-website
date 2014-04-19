5ede9706da5ba4012dda8db08bc0a08acd4098aa
drecode feil <drecodeam@gmail.com>
2014-04-19 20:48:00 +0530

    new UI

--- js/home.js
+++ js/home.js
@@ -1,13 +1,61 @@
-$(document).ready(function(){
-	var firebase = new Firebase('https://osdconf.firebaseio.com/notifyusers');
-	$('#notify-me').click(function() {
-		$('#notify-box').slideDown();
-		var email = $('#notify-email').val();
+$( document ).ready( function(){
+    // var firebase = new Firebase( 'https://osdconf.firebaseio.com/notifyusers' );
+    // $( '#notify-me' ).click(function() {
+    //     $('#notify-box').slideDown();
+    //     var email = $('#notify-email').val();
 
-		if (email.length) {
-			firebase.push({email: email});
-			$('#notify-box').html('<p>Thank you! You will hear from us soon.</p>');
-		}
+    //     if (email.length) {
+    //         firebase.push({email: email});
+    //         $('#notify-box').html('<p>Thank you! You will hear from us soon.</p>');
+    //     }
 
-	});
+    // });
+
+    var isMapLoaded = false,
+        isMapReady = false
+        isMapLoadQueue = false;
+
+
+    function initialize() {
+        console.log( 'called' );
+        var JIIT=new google.maps.LatLng( 28.630298, 77.372050 );
+        console.log( JIIT );
+        var stylesArray = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}];
+        var mapOptions = {
+            center    : JIIT,
+            zoom      : 15,
+            mapTypeId : google.maps.MapTypeId.ROADMAP,
+            styles    : stylesArray
+        };
+        var map = new google.maps.Map( $( '#map-container' ), mapOptions );
+        console.log( map );
+
+        var marker= new google.maps.Marker( {
+            position  : JIIT,
+            map       : map,
+            title     : "Jaypee Institute of Information Technology"
+        });
+    }
+
+    google.maps.event.addDomListener( window, 'load', function() {
+        isMapReady = true;
+        if ( isMapLoadQueue ) {
+            initialize();
+        }
+    });
+
+    $( '.js-map-open' ).on( 'click', function() {
+        $( '.js-map-container' ).toggle();
+        if ( isMapReady ) {
+            if ( !isMapLoaded ) {
+                initialize();
+            }
+        } else {
+            isMapLoadQueue = true;
+        }
+        return false;
+    });
+
+
+
 });
---
+++ message.md
@@ -0,0 +1,9 @@
+We are a bunch of [crazy people](https://github.com/orgs/osdc/members) who love to hack and collaborate. By obvious nature, we also love open source and have been contributing to it since we got introduced to it. We love going to conferences and hackathons, meet new people, discuss thier workflows and hack with them to build something.
+
+We have learned a lot from people who have pushed us forward, and want to do the same for other people, hence we decided to organize a small conference for people like us - **OSDConf**. With help from zomato, Wikimedia, mozilla and GitHub we have been able to arrange this conference. Wether you are a beginner or a pro, it does not matter to us, as long as you are willing to learn and collaborate, there is definitely going to be something for you there.
+
+The conference is on 24th April at Jaypee Institute of Information Technology [show on map](#). We have some [sessions]() already planned up, and if you want to give one of your own, [propose your session here](). If you are not the session kind of people, we have hackspaces for you.
+
+That feeling that you get when you hit that *git push* command after gulping down your beer - If you know that feeling, you should definitely come over, but then again if you don't know the feeling, you should absolutely come over to know what it feels like.
+
+
