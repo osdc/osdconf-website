db8d8c7995ee813ef1a93ba905c8dc632313d33b
Ujjwal Sharma <usharma1998@gmail.com>
2018-01-11 23:51:22 +0530

    Clean CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('serve', () => {
   browserSync.init({
@@ -7,6 +8,17 @@ gulp.task('serve', () => {
   });
 
   gulp.watch('app/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('app/assets/styles/*.css')
+    .pipe(cleanCSS({
+      debug: true
+    }, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['serve']);
